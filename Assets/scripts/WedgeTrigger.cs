7991db0b458b5ecef2b74451bd1658f6b0fda340
Nea Pulkkinen <fnepu005@edu.xamk.fi>
2025-02-11 18:20:21 +0200

    homework 3
    started and finished homework

--- Assets/scripts/WedgeTrigger.cs
+++ Assets/scripts/WedgeTrigger.cs
@@ -7,7 +7,7 @@ public class WedgeTrigger : MonoBehaviour
 {
     //modify at the end
     [Range(0.1f, 20f)]
-    public float radius = 5.0f;
+    public float radius = 0.5f;
 
     [Range(1, 180f)]
     public float threshold = 45f;
@@ -16,6 +16,8 @@ public class WedgeTrigger : MonoBehaviour
     public GameObject lookAtObject;
 
     private Color orange = new Color32(0xF0, 0xA5, 0x00, 0xA0);
+
+    public float offPos = 5.0f;
 
     private void OnDrawGizmos()
     {
@@ -29,34 +31,43 @@ public class WedgeTrigger : MonoBehaviour
         Vector3 normalizedV = v.normalized;
         Vector3 normalizedL = l.normalized;
 
-        //draw vectors
+        //draw vectors (player & enemy)
         MyDraw.DrawVectorAt(vecTriggerPos, v, Color.magenta, 3.0f);
-        MyDraw.DrawVectorAt(vecTriggerPos, l, orange, 4.0f);  //0x in the beginning
+        MyDraw.DrawVectorAt(vecTriggerPos, normalizedL * radius, orange, 4.0f);  //0x in the beginning
 
         //maths
         float dotProduct = Vector3.Dot(normalizedV, normalizedL);
         float angle = Mathf.Acos(dotProduct) * Mathf.Rad2Deg;
 
-        //check if player is in the area
-        Color playerColor = angle >= threshold ? Color.green : Color.red;
-
-        if (v.magnitude <= radius && angle < threshold)
-        {
-            Handles.color = Color.red;
-        } else Handles.color = Color.green;
-
         //enemy look direction/area
         Quaternion rotate = Quaternion.Euler(0, threshold, 0);
         Vector3 rightRotation = rotate * normalizedL;  //right border
-        MyDraw.DrawVectorAt(transform.position, rightRotation * radius, playerColor);
 
         rotate = Quaternion.Euler(0, -threshold, 0);
         Vector3 leftRotation = rotate * normalizedL;  //left border
-        MyDraw.DrawVectorAt(transform.position, leftRotation * radius, playerColor);
 
 
         //the cheese piece -og-
-        Handles.DrawWireArc(vecTriggerPos, new Vector3(0, 1, 0), leftRotation, threshold * 2, radius, 3.0f);
+        Vector3 offsetTriggerPosUp = new(vecTriggerPos.x, vecTriggerPos.y + offPos, vecTriggerPos.z);
+        Vector3 offsetTriggerPosDown = new(vecTriggerPos.x, vecTriggerPos.y - offPos, vecTriggerPos.z);
+
+        //check if player is in the area
+        if (v.magnitude <= radius && angle < threshold && offsetTriggerPosUp.y > vecTargetPos.y && offsetTriggerPosDown.y < vecTargetPos.y)
+        {
+            Handles.color = Color.red;
+        }
+        else Handles.color = Color.white;
+
+        Handles.DrawLine(offsetTriggerPosUp, offsetTriggerPosUp + rightRotation * radius, 3.0f);
+        Handles.DrawLine(offsetTriggerPosUp, offsetTriggerPosUp + leftRotation * radius, 3.0f);
+        Handles.DrawWireArc(offsetTriggerPosUp, new Vector3(0, 1, 0), leftRotation, threshold * 2, radius, 3.0f);
+        Handles.DrawLine(offsetTriggerPosDown, offsetTriggerPosDown + rightRotation * radius, 3.0f);
+        Handles.DrawLine(offsetTriggerPosDown, offsetTriggerPosDown + leftRotation * radius, 3.0f);
+        Handles.DrawWireArc(offsetTriggerPosDown, new Vector3(0, 1, 0), leftRotation, threshold * 2, radius, 3.0f);
+
+        Handles.DrawLine(offsetTriggerPosDown, offsetTriggerPosUp, 3.0f);
+        Handles.DrawLine(offsetTriggerPosUp + rightRotation * radius, offsetTriggerPosDown + rightRotation * radius, 3.0f);
+        Handles.DrawLine(offsetTriggerPosUp + leftRotation * radius, offsetTriggerPosDown + leftRotation * radius, 3.0f);
     }
 }
 

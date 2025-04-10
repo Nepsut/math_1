d12f574980b99ba1da853f1929700b97c7bb8e86
Nea Pulkkinen <fnepu005@edu.xamk.fi>
2025-04-10 10:59:30 +0300

    Final assignment started

--- Assets/scripts/VectorMath/Laser.cs
+++ Assets/scripts/VectorMath/Laser.cs
@@ -5,40 +5,28 @@ using UnityEngine;
 
 public class Laser : MonoBehaviour
 {
+    [Range(0, 10)]
+    [SerializeField] private int reflectionNumber = 3;
+
     private void OnDrawGizmos()
     {
         Handles.color = Color.red;
-        Handles.DrawLine(transform.position, transform.position + transform.right * 5f, 3f);
+        Gizmos.color = Color.red;
 
-        RaycastHit hit;
+        Vector3 prevPoint = transform.position;
+        Vector3 lastRefDir = transform.right;
 
-        if (Physics.Raycast(transform.position, transform.right, out hit))
+        for (int i = 0; i <= reflectionNumber; i++) 
         {
-            //if we hit something
-            Gizmos.color = Color.red;
+            if (!Physics.Raycast(prevPoint, lastRefDir, out RaycastHit hit)) break;
+            Vector3 reflection = Vector3.Reflect(lastRefDir, hit.normal);
+
+            Handles.DrawLine(prevPoint, hit.point, 3.0f);
             Gizmos.DrawSphere(hit.point, 0.1f);
 
-            //normal
-            MyDraw.DrawVectorAt(hit.point, 3f * hit.normal, Color.green, 3f);
-
-            //reflection
-            Vector3 reflection = transform.right - 2 * (Vector3.Dot(transform.right, hit.normal)) * hit.normal;
-
-            MyDraw.DrawVectorAt(hit.point, 2.5f * reflection, Color.red, 3f); //or Vector3.Reflect()
-
-            //Vector3 reflection2 = Vector3.Reflect(transform.right, hit.point); //??????
+            prevPoint = hit.point;
+            lastRefDir = reflection;
         }
-    }
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }

34485d270a09f624b476be3a3a7d0e151d46cb1c
Nea Pulkkinen <fnepu005@edu.xamk.fi>
2025-04-10 09:48:15 +0300

    homework 11
    did the thing and added some assets ready for the final assignment

--- Assets/scripts/VectorMath/Positioning.cs
+++ Assets/scripts/VectorMath/Positioning.cs
@@ -1,9 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using Unity.Mathematics;
 using UnityEngine;
 
 public class Positioning : MonoBehaviour
 {
+
+    [SerializeField] private Transform car;
+
     private void OnDrawGizmos()
     {
         RaycastHit hit;
@@ -21,23 +26,15 @@ public class Positioning : MonoBehaviour
             MyDraw.DrawVectorAt(hit.point, 1f * transform.right, Color.magenta, 4f);
 
             //use cross-product to get "right"-vector and draw it
-            Vector3 right = Vector3.Cross(hit.normal, transform.right);
+            Vector3 right = Vector3.Cross(hit.normal, transform.right).normalized;
             MyDraw.DrawVectorAt(hit.point, 1f * right, Color.red, 4f);
 
             //use cross-product to get "forward"-vector and draw it
             Vector3 forward = Vector3.Cross(right, hit.normal);
             MyDraw.DrawVectorAt(hit.point, 1f * forward, Color.blue, 4f);
-        }
 
-        // Start is called before the first frame update
-        void Start()
-        {
 
-        }
-
-        // Update is called once per frame
-        void Update()
-        {
+            car.SetLocalPositionAndRotation(hit.point, quaternion.LookRotation(right, hit.normal));
 
         }
     }
